844596421917cce178c2c2cf3484b1f0e4f8a558
SerjAnri <serj-anri.bmth@yandex.ru>
2024-04-14 19:44:57 +0300

    initial commit

---
+++ src/test/kotlin/com/example/teamselection/TeamSelectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.teamselection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamSelectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
